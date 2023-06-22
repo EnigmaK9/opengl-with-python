0206219f80b6ed49c2ddfc05f2a852649c03fb67
Ignacio Padilla <enigmak9@protonmail.com>
2023-06-22 04:27:21 -0600

    corrected indentation

--- 01-intro/game.py
+++ 01-intro/game.py
@@ -3,21 +3,20 @@ from OpenGL.GL import *
 
 class App:
     def __init__(self):
-            #initialize python
-            pg.init()
-            pg.display.set_mode((640,480),pg.OPENGL|pg.DOUBLEBUF)
-            self.clock = pg.time.Clock()
-            #initialize OpenGL
-            glClearColor(0.1,0.2,0.2,1)
-            self.mainLoop()
+        #initialize python
+        pg.init()
+        pg.display.set_mode((640,480),pg.OPENGL|pg.DOUBLEBUF)
+        self.clock = pg.time.Clock()
+        #initialize OpenGL
+        glClearColor(0.1,0.2,0.2,1)
+        self.mainLoop()
 
     def mainLoop(self):
-        
         running = True
-        while (running):
+        while running:
             #check events
             for event in pg.event.get():
-                if (event.type == pg.quit):
+                if event.type == pg.QUIT:
                     running = False
             #refresh screen
             glClear(GL_COLOR_BUFFER_BIT)
@@ -29,5 +28,6 @@ class App:
 
     def quit(self):
         pg.quit()
-    if __name__ == "__main__":
-        myApp = App()
+
+if __name__ == "__main__":
+    myApp = App()
