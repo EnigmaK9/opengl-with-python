8112077b92cb3015463e78e977d5027337ad8951
Ignacio Padilla <enigmak9@protonmail.com>
2023-06-22 03:45:07 -0600

    added readme debugging game.py

--- 01-intro/game.py
+++ 01-intro/game.py
@@ -5,29 +5,29 @@ class App:
     def __init__(self):
             #initialize python
             pg.init()
-            pg.display.set_mode(640,480),pg.OPENGL|pg.DOUBLEBUF)
+            pg.display.set_mode((640,480),pg.OPENGL|pg.DOUBLEBUF)
             self.clock = pg.time.Clock()
             #initialize OpenGL
             glClearColor(0.1,0.2,0.2,1)
             self.mainLoop()
 
-        def mainLoop(self):
-            
-            running = True
-            while (running):
-                #check events
-                for event in pg.event.get():
-                    if (event.type == pg.quit):
-                        running = False
-                #refresh screen
-                glClear(GL_COLOR_BUFFER_BIT)
-                pg.display.flip()
+    def mainLoop(self):
+        
+        running = True
+        while (running):
+            #check events
+            for event in pg.event.get():
+                if (event.type == pg.quit):
+                    running = False
+            #refresh screen
+            glClear(GL_COLOR_BUFFER_BIT)
+            pg.display.flip()
 
-                #timing
-                self.clock.tick(60)
-            self.quit()
+            #timing
+            self.clock.tick(60)
+        self.quit()
 
-        def quit(self):
-            pg.quit()
+    def quit(self):
+        pg.quit()
     if __name__ == "__main__":
         myApp = App()
